8f91a87ae7c72d6a56a9fd6c7728b6c8f3b037a6
Tarek Saier <tareksaier@gmail.com>
2016-06-08 22:03:58 +0900

    tarek3に３つ目の長方形を

--- love/tarek3/main.lua
+++ love/tarek3/main.lua
@@ -32,8 +32,9 @@ function Rectangle:goLeft()
 end
 
 -- ### globals
-rect1 = Rectangle:new(30, 30, 100, 100)
-rect2 = Rectangle:new(10, 10, 300, 10)
+rect1 = Rectangle:new(30, 30, 150, 120)
+rect2 = Rectangle:new(30, 30, 185, 120)
+rect3 = Rectangle:new(10, 10, 300, 10)
 -- ### /globals
 
 -- ### callback functions
@@ -44,25 +45,31 @@ function love.load()
 end
 
 function love.update(dt)
-    rect2.yPos = (rect2.yPos + 1) % 300
+    rect3.yPos = (rect3.yPos + 1) % 300
 end
 
 function love.draw()
     love.graphics.print("Position: (" .. rect1.xPos .. "|" .. rect1.yPos .. ").", 20, 20)
+    love.graphics.print("Position: (" .. rect2.xPos .. "|" .. rect2.yPos .. ").", 20, 40)
     rect1:draw()
     rect2:draw()
+    rect3:draw()
 end
 
 function love.keypressed(key, scancode)
     if scancode ~= nil then
         if scancode == "up" then
             rect1:goUp()
+            rect2:goDown()
         elseif scancode == "down" then
             rect1:goDown()
+            rect2:goUp()
         elseif scancode == "right" then
             rect1:goRight()
+            rect2:goLeft()
         elseif scancode == "left" then
             rect1:goLeft()
+            rect2:goRight()
         end
     end
 end
